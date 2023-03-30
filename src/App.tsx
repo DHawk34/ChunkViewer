a600a33d9463480ae33785657c4943c58b82d792
DHawk34 <neobuild@mail.ru>
2023-03-30 15:13:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 import { ImageContainer } from "./components/ImageContainer/ImageContainer";
 import { ChunkContainer } from "./components/ChunkContainer/ChunkContainer";
+import chunkHandler, { ReadSettings } from "./scripts/chunkHandler";
+import { text } from "stream/consumers";
 
 export type ChunkType = {
   name: string,
@@ -33,10 +35,24 @@ export class App extends React.Component<{}, AppState>{
     this.setState({ chunkArray: newChunkArray })
   }
 
+  loadChunks = (path: string) => {
+    console.log("load chunks")
+    chunkHandler.readChunks(path, new ReadSettings(false, false), (chunks) =>{
+      let result: { name: string; text: string; }[] = []
+      for (let i = 0; i < chunks.length; i++) {
+        const chunk = chunks[i];
+        console.log(chunk)
+        result.push({name:chunk.name, text:chunk.value.toString()})
+      }
+
+      this.updateChunkArray(result)
+    })
+  }
+
   render(): React.ReactNode {
     return (
       <div id="#container">
-        <ImageContainer OnImageLoaded={() => { }} />
+        <ImageContainer OnImageLoaded={this.loadChunks} />
         <ChunkContainer chunkArray={this.state.chunkArray} OnChunksUpdated={this.updateChunkArray} />
         <div id="toolbar_container">
           <button className="toolbar_button">Test</button>
