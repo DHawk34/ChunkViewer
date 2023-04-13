8a61e27dfda985712d1b90f0c0e0c5902b85e8cd
CompleXss <max.petrov.27@mail.ru>
2023-04-13 15:34:44 +0300

    sdParamParser created (not done)

---
+++ src/scripts/sdParamParser.ts
@@ -0,0 +1,51 @@
+import { Chunk } from "./chunks/chunkReader";
+
+const NEGATIVE_PROMPT_TEXT = "Negative prompt: ";
+const STEPS_TEXT = "Steps: ";
+
+interface Parameters {
+    [key: string]: string
+}
+
+export function parseParameters(parameters: string): Parameters | string {
+    const backUp = parameters;
+    let result: Parameters = {};
+    parameters = parameters.replaceAll('\n', ''); // regex: replace(/\n/g, "")
+
+    if (parameters.length == 0) {
+        return backUp;
+    }
+
+    // Get positive & negative prompt
+    let positive = "";
+    let negative = "";
+
+    let out = parameters.split(STEPS_TEXT);
+    let posNeg = out[0];
+    parameters = out[1];
+
+    if (posNeg.includes(NEGATIVE_PROMPT_TEXT)) {
+        out = posNeg.split(NEGATIVE_PROMPT_TEXT);
+        positive = out[0];
+        negative = out[1];
+    }
+    else {
+        positive = posNeg;
+        negative = "";
+    }
+
+    result["positive"] = positive;
+    result["negative"] = negative;
+
+    // Get other parameters
+    let params = parameters.split(", ");
+    params[0] = STEPS_TEXT + String(params[0]); // Возвращаем на место "Steps: "
+
+    for (let i = 0; i < params.length; i++) {
+        let out = params[i].split(": ");
+        let name = out[0];
+        result[name] = out[1];
+    }
+
+    return result;
+}
