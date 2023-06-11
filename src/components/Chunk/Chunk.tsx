ea05d40b0bcae2d13bd9df48fa7c42599d3a1be8
DHawk34 <neobuild@mail.ru>
2023-06-11 17:17:15 +0300

    add chunk paramaters table

--- src/components/Chunk/Chunk.tsx
+++ src/components/Chunk/Chunk.tsx
@@ -4,8 +4,9 @@ import { appWindow } from "@tauri-apps/api/window";
 import { save } from '@tauri-apps/api/dialog';
 import { event } from '@tauri-apps/api';
 import chunkHandler from '../../scripts/chunks/chunkHandler';
-import { Parameters } from '../../scripts/sdParamParser';
+import { Parameters, parseParameters } from '../../scripts/sdParamParser';
 import { Draggable, DraggableStateSnapshot, DraggingStyle, NotDraggingStyle } from 'react-beautiful-dnd';
+import { objectToArray } from '../../scripts/utils';
 
 type MyProps = {
     // using `interface` is also ok
@@ -15,11 +16,18 @@ type MyProps = {
     OnDelete: (index: number) => void
 };
 
-export class Chunk extends React.Component<MyProps, {}> {
+type MyState = {
+    showParameters: boolean
+}
+
+export class Chunk extends React.Component<MyProps, MyState> {
 
     curInput: HTMLElement | undefined | null;
     constructor(props: MyProps) {
         super(props)
+
+        this.state = { showParameters: false }
+
         appWindow.listen("tauri://resize", (event) => {
             if (this.curInput != null) {
                 this.autoGrow(this.curInput)
@@ -44,13 +52,14 @@ export class Chunk extends React.Component<MyProps, {}> {
         input.value = val;
         input.onblur = () => {
             var val = input.value;
-            element.textContent = val;
 
-            if (element.className == 'chunk_name') {
-                this.props.chunk.name = input.value;
+            if (element.className === 'chunk_name') {
+                element.textContent = val.trim();
+                this.props.chunk.name = val.trim();
                 this.props.OnUpdate(index, this.props.chunk)
             }
             else {
+                element.textContent = val;
                 this.props.chunk.value = input.value;
                 this.props.OnUpdate(index, this.props.chunk)
             }
@@ -72,24 +81,51 @@ export class Chunk extends React.Component<MyProps, {}> {
         element.style.height = (element.scrollHeight) + "px";
     }
 
+    changeView = () => {
+        this.setState({ showParameters: !this.state.showParameters })
+        // parseParameters
+    }
+
     exportChunk = () => {
         chunkHandler.exportChunk(this.props.chunk)
             .catch((error) => console.log(error));
     }
+    
 
     render(): React.ReactNode {
+
+        let parameters = objectToArray<string>(parseParameters(this.props.chunk.value))?.map((param: { key: string, value: string}, index: number) => {
+            return <tr key={index}>
+                <td className='param_name'>{param.key}</td>
+                <td className='param_text'>{param.value}</td>
+            </tr>
+        })
+
         return (
             <Draggable draggableId={this.props.index.toString()} key={this.props.index} index={this.props.index}>
                 {(provided, snapshot) => (
                     <div className="chunk" key={this.props.index}
                         {...provided.draggableProps} ref={provided.innerRef}>
                         <div className="chunk_header">
-                            <div  {...provided.dragHandleProps} id='chunk_dragger'><svg width={16} height={35} fill='white' viewBox="64 0 128 256"><rect width="128" height="256" fill="none" /><circle cx="92" cy="60" r="12" /><circle cx="164" cy="60" r="12" /><circle cx="92" cy="128" r="12" /><circle cx="164" cy="128" r="12" /><circle cx="92" cy="196" r="12" /><circle cx="164" cy="196" r="12" /></svg></div>
+                            <div  {...provided.dragHandleProps} id='chunk_dragger'><svg width={16} height={35} fill='white' viewBox="64 -11.5 128 256"><rect width="128" height="256" fill="none" /><circle cx="92" cy="60" r="12" /><circle cx="164" cy="60" r="12" /><circle cx="92" cy="128" r="12" /><circle cx="164" cy="128" r="12" /><circle cx="92" cy="196" r="12" /><circle cx="164" cy="196" r="12" /></svg></div>
                             <div className="chunk_name" onDoubleClick={(e) => this.spawnInput(e.currentTarget, this.props.index)}>{this.props.chunk.name}</div>
+                            {
+                                this.props.chunk.name === 'parameters' &&
+                                <svg className='change_view_chunk_button button' width="35" height="35" viewBox="0 0 109.927 122.881" onClick={this.changeView}><g><path fill='currentColor' d="M38.884,13.221c-0.041,1.756,0.255,2.601,1.781,2.603h36.71 c6.647,0.521,12.596,2.272,17.625,5.684c3.492,2.369,6.04,5.183,7.963,8.302c3.412,5.537,5.34,13.521,6.384,19.967 c0.45,2.787,0.661,5.392,0.18,6.6c-0.228,0.569-0.565,0.953-1.023,1.135c-2.524,1.011-5.192-2.464-6.614-4.127 c-6.77-7.918-16.61-11.194-28.212-11.809h-33.56c-1.201,0.208-1.627,1.096-1.507,2.466v9.451c-0.07,3.141-1.654,4.21-4.794,3.15 L4.92,33.966l-2.842-2.231l-0.727-0.57c-2.323-2.089-1.368-3.991,0.717-5.633l2.206-1.736L31.964,1.999 c3.379-2.661,6.92-3.373,6.92,2.362V13.221L38.884,13.221z M71.042,109.66c0.041-1.757-0.254-2.602-1.78-2.604h-36.71 c-6.647-0.521-12.596-2.271-17.625-5.684c-3.514-2.384-6.072-5.217-7.998-8.358c-3.485-5.686-5.757-14.941-6.591-21.583 c-0.266-2.119-0.321-3.966,0.063-4.927c0.227-0.569,0.565-0.953,1.022-1.136c2.524-1.011,5.192,2.464,6.614,4.127 c6.771,7.918,16.611,11.194,28.213,11.809h33.56c1.2-0.207,1.627-1.096,1.507-2.466v-9.451c0.07-3.141,1.654-4.21,4.794-3.15 l28.896,22.677l2.843,2.231l0.727,0.57c2.323,2.089,1.367,3.991-0.718,5.633l-2.205,1.736l-27.689,21.797 c-3.38,2.661-6.921,3.373-6.921-2.362V109.66L71.042,109.66z" /></g></svg>
+                            }
                             <svg className="export_chunk_button button" onClick={this.exportChunk} width="35" height="35" viewBox="0 0 35 35"><path d="M4.4,29.2c3.6-5.4,8.8-7.9,16-7.9v6.4l10.2-10.9L20.4,5.8v6.2C10.2,13.6,5.8,21.4,4.4,29.2z" fill="currentColor"></path></svg>
                             <svg className="delete_chunk_button button" onClick={this.deleteChunk} aria-hidden="true" role="img" width="35" height="35" viewBox="0 0 24 24"><path fill="currentColor" d="M15 3.999V2H9V3.999H3V5.999H21V3.999H15Z"></path><path fill="currentColor" d="M5 6.99902V18.999C5 20.101 5.897 20.999 7 20.999H17C18.103 20.999 19 20.101 19 18.999V6.99902H5ZM11 17H9V11H11V17ZM15 17H13V11H15V17Z"></path></svg>
                         </div>
-                        <p className="chunk_text" onDoubleClick={(e) => this.spawnInput(e.currentTarget, this.props.index)}>{this.props.chunk.value.toString()}</p>
+                        {
+                            this.state.showParameters && this.props.chunk.name === 'parameters' ?
+                                <table id='parameters_table'>
+                                    <tbody>
+                                        {parameters}
+                                    </tbody>
+                                </table>
+                                :
+                                <p className="chunk_text" onDoubleClick={(e) => this.spawnInput(e.currentTarget, this.props.index)}>{this.props.chunk.value.toString()}</p>
+                        }
                     </div>
                 )}
             </Draggable>
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -2,3 +2,12 @@ export function swap<T>(arr: T[], index1: number, index2: number) {
     const [oneElement] = arr.splice(index1, 1)
     arr.splice(index2, 0, oneElement)
 }
+
+export function objectToArray<T>(object: Object) {
+    let keys = Object.keys(object);
+    let values = Object.values(object) as T[];
+
+    return keys.map((key: string, index: number) => {
+        return { key: key, value: values[index] }
+    });
+}
