5364462cf4f7282883a84252d70ddca1fd70739e
CompleXss <max.petrov.27@mail.ru>
2023-06-11 18:59:50 +0300

    add isDigit func

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -4,10 +4,14 @@ export function swap<T>(arr: T[], index1: number, index2: number) {
 }
 
 export function objectToArray<T>(object: Object) {
-    let keys = Object.keys(object);
-    let values = Object.values(object) as T[];
+    let keys = Object.keys(object)
+    let values = Object.values(object) as T[]
 
     return keys.map((key: string, index: number) => {
         return { key: key, value: values[index] }
-    });
+    })
+}
+
+export function isDigit(char: string): boolean {
+    return char >= '0' && char <= '9'
 }
