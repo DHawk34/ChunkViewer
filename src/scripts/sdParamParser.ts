a22b9894d4d1c133f19675418c4ed8d26c59f17b
CompleXss <max.petrov.27@mail.ru>
2023-07-01 00:24:07 +0300

    Update sdParamParser.ts

--- src/scripts/sdParamParser.ts
+++ src/scripts/sdParamParser.ts
@@ -12,7 +12,7 @@ export type Parameters = {
 }
 
 export function parseParameters(parameters: string): Parameters {
-    let result: Parameters = {}
+    const result: Parameters = {}
     parameters = parameters.replaceAll('\n', '') // regex: replace(/\n/g, "")
 
     if (parameters.length === 0) {
@@ -21,14 +21,16 @@ export function parseParameters(parameters: string): Parameters {
 
     // Get positive & negative prompt
     retreivePosNegPrompt()
-
-    // Get other parameters
-    const params = parameters.split(', ')
+    
+    //const params = parameters.split(', ')
+    const params = mySplit(parameters, ', ')
 
     for (let i = 0; i < params.length; i++) {
-        let out = params[i].split(': ')
-        let name = out[0].trim()
-        result[name] = out[1]
+        const index = params[i].indexOf(': ')
+        const name = params[i].substring(0, index).trim()
+        const value = params[i].substring(index + 2).replaceAll('"', '')
+
+        result[name] = value
     }
 
     return result
@@ -43,13 +45,13 @@ export function parseParameters(parameters: string): Parameters {
         const promptEndIndex = indexOfPromptEnd(parameters)
         if (promptEndIndex === -1) return
 
-        let posNeg = parameters.slice(0, promptEndIndex)
+        const posNeg = parameters.slice(0, promptEndIndex)
         parameters = parameters.slice(promptEndIndex)
 
         if (posNeg.includes(NEGATIVE_PROMPT_TEXT)) {
-            let out = posNeg.split(NEGATIVE_PROMPT_TEXT)
-            positive = out[0]
-            negative = out[1]
+            const index = posNeg.indexOf(NEGATIVE_PROMPT_TEXT)
+            positive = posNeg.substring(0, index)
+            negative = posNeg.substring(index + NEGATIVE_PROMPT_TEXT.length)
         }
         else {
             positive = posNeg
@@ -58,6 +60,48 @@ export function parseParameters(parameters: string): Parameters {
 
         result['Positive prompt'] = positive
         result['Negative prompt'] = negative
+    }
+}
+
+
+
+function mySplit(text: string, separator: string): string[] {
+    const result: string[] = []
+    const separatorLength = separator.length
+    const checkLength = text.length - separatorLength
+
+    let blockStarted: boolean = false
+    let startIndex = 0
+
+    for (let i = 0; i < checkLength; i++) {
+        if (text[i] === '"') {
+            blockStarted = !blockStarted
+            continue
+        }
+        if (blockStarted) continue
+
+        if (isSeparator(i)) {
+            result.push(text.substring(startIndex, i))
+            startIndex = i + separatorLength
+        }
+    }
+
+    const last = text.substring(startIndex)
+    if (last && last !== '') {
+        result.push(last)
+    }
+
+    return result
+
+
+
+    function isSeparator(i: number): boolean {
+        for (let j = 0; j < separatorLength; j++) {
+            if (text[i + j] !== separator[j])
+                return false
+        }
+
+        return true
     }
 }
 
