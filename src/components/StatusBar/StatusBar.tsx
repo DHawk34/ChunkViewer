30bccd74dbe1f50fefe32006640695dec5d45729
DHawk34 <neobuild@mail.ru>
2023-11-11 23:23:45 +0300

    add Warning

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { LogMessage } from "../../scripts/hooks/useLoggerHook";
+import { LogMessage, useLogger } from "../../scripts/hooks/useLoggerHook";
 import './StatusBar.css'
 
 export function StatusBar(props: { logs: LogMessage[] }) {
     const [clicked, setClicked] = useState<boolean>(false)
+    const [showWarning, setShowWarning] = useState<boolean>(true);
+    const logger = useLogger()
 
     function toggleLogs() {
         setClicked(!clicked)
@@ -15,6 +17,7 @@ export function StatusBar(props: { logs: LogMessage[] }) {
         }
         else {
             container?.classList.add('full_open')
+            setShowWarning(false)
         }
     }
 
@@ -34,6 +37,7 @@ export function StatusBar(props: { logs: LogMessage[] }) {
                     getLastMessage()
                 }
             </div>
+            {showWarning ? <svg id="warning_icon" color = "red" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 4a8 8 0 1 0 0 16 8 8 0 0 0 0-16zM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12z" fill="red"></path><path d="M12 14a1 1 0 0 1-1-1V7a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1zm-1.5 2.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z" fill="red"></path></svg> : <></>}
             <button onClick={toggleLogs}>{clicked ?
                 <svg height="32px" width="32px" viewBox="0 0 32 32"> <path d="M14.8,20.8l-9.6-9.6c-0.9-0.9-0.9-2.3,0-3.2L6,7.2c0.9-0.9,2.3-0.9,3.2,0l7.3,7.3l7.3-7.3 c0.9-0.9,2.3-0.9,3.2,0L27.8,8c0.9,0.9,0.9,2.3,0,3.2l-9.6,9.6c-0.5,0.5-1.1,0.7-1.7,0.6C15.9,21.5,15.2,21.3,14.8,20.8z" fill="#ffffff" /></svg>
                 :
