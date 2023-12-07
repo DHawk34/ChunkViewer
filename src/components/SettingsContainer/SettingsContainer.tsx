54fd8e5a48a7ccaac4d605c72cf804ccef6e4c14
CompleXss <max.petrov.27@mail.ru>
2023-12-07 23:53:01 +0300

    add settings page

---
+++ src/components/SettingsContainer/SettingsContainer.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from "react";
+import { ChunkTypes } from "../../scripts/chunks/chunkSaver";
+import { settingsManager } from "../../scripts/settings";
+import './SettingsContainer.css'
+
+export function SettingsContainer() {
+    const comboBox = useRef<HTMLSelectElement>(null)
+    const checkBox = useRef<HTMLInputElement>(null)
+
+    // init state from settings
+    useEffect(() => {
+        if (checkBox.current) checkBox.current.checked = get_allowUnsafeChunkNames()
+        if (comboBox.current) comboBox.current.value = get_chunkType()
+    }, [])
+
+    return (
+        <div id='settings_container'>
+            <label>Chunk type when saving</label>
+            
+            <select ref={comboBox} onChange={update_chunkType}>
+                <option value={ChunkTypes.tEXt}> {ChunkTypes.tEXt} (default) </option>
+                <option value={ChunkTypes.iTXt}> {ChunkTypes.iTXt} </option>
+                <option value={ChunkTypes.zTXt}> {ChunkTypes.zTXt} </option>
+            </select>
+
+            <hr />
+
+            <label>
+                <input ref={checkBox} type="checkbox" onChange={update_allowUnsafeChunkNames} />
+                Allow <span className="tooltip">unsafe
+                    <span className="tooltiptext">If chunk name is 4-character, save it as code rather than text</span>
+                </span> chunk names
+            </label>
+        </div>
+    )
+}
+
+
+
+// chunk type
+function get_chunkType() {
+    return settingsManager.getCache('chunkType')
+}
+
+function update_chunkType(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value as ChunkTypes
+    settingsManager.setCache('chunkType', value)
+}
+
+// unsafe chunk names
+function get_allowUnsafeChunkNames() {
+    return settingsManager.getCache('allowUnsafeChunkNames')
+}
+
+function update_allowUnsafeChunkNames(e: React.ChangeEvent<HTMLInputElement>) {
+    const checked: boolean = e.target.checked
+    settingsManager.setCache('allowUnsafeChunkNames', checked)
+}
--- src/components/ToolbarContainer/ToolbarContainer.tsx
+++ src/components/ToolbarContainer/ToolbarContainer.tsx
@@ -1,19 +1,20 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { FeaturesContainer, FeaturesProps } from '../FeaturesContainer/FeaturesContainer';
+import { SettingsContainer } from "../SettingsContainer/SettingsContainer";
+import { getVersion } from '@tauri-apps/api/app';
 import './ToolbarContainer.css'
 
-import { getVersion } from '@tauri-apps/api/app';
-const appVersion = await getVersion();
+const appVersion = await getVersion()
 
 export function ToolbarContainer(props: FeaturesProps) {
     return (
-        <div id="toolbar_container">
+        <div id='toolbar_container'>
             <Tabs>
                 <TabList>
                     <Tab>Features</Tab>
                     <Tab>Options</Tab>
                 </TabList>
-                
+
                 <TabPanel>
                     <FeaturesContainer
                         chunkArray={props.chunkArray}
@@ -23,10 +24,10 @@ export function ToolbarContainer(props: FeaturesProps) {
                 </TabPanel>
 
                 <TabPanel>
-                    <p>Comming soon!</p>
+                    <SettingsContainer />
                 </TabPanel>
             </Tabs>
-            <p id="version_label">V.{appVersion}</p>
+            <p id='version_label'>v {appVersion}</p>
         </div>
     )
 }
