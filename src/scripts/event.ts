7d5d3e99057e36f7bacf840b14a119324b54e68e
CompleXss <max.petrov.27@mail.ru>
2023-12-09 03:11:13 +0300

    make Action builder-like

--- src/scripts/event.ts
+++ src/scripts/event.ts
@@ -1,23 +1,26 @@
 export type EventHandler<T> = (event: T) => void
 
 export interface Action<T> {
-    register(handler: EventHandler<T>): void
-    unregister(handler: EventHandler<T>): void
+    register(handler: EventHandler<T>): Action<T>
+    unregister(handler: EventHandler<T>): Action<T>
 }
 
 export class EventDispatcher<T> implements Action<T> {
+    /** @internal */
     private handlers: EventHandler<T>[] = []
 
-    public register(handler: EventHandler<T>): void {
+    public register(handler: EventHandler<T>): Action<T> {
         this.handlers.push(handler)
+        return this
     }
 
-    public unregister(handler: EventHandler<T>): void {
+    public unregister(handler: EventHandler<T>): Action<T> {
         for (let i = 0; i < this.handlers.length; i++) {
             if (this.handlers[i] === handler) {
                 this.handlers.splice(i, 1)
             }
         }
+        return this
     }
 
     public dispatch(value: T): void {
