f95aa06149307fa345ab043ad12d0fb37cbaa43b
DHawk34 <neobuild@mail.ru>
2024-07-25 16:27:03 +0300

    update ParamsTableWithExpandle

--- src/components/ParamsTableWithExpandle/ParamsTableWithExpandle.tsx
+++ src/components/ParamsTableWithExpandle/ParamsTableWithExpandle.tsx
@@ -1,40 +1,101 @@
-import { isObject, Dictionary } from '@/scripts/utils'
-import './ParamsTableWithExpandle.css'
+import { isObject, Dictionary, capitalizeFirstLetter } from '@/scripts/utils'
+import { forwardRef, useEffect, useRef, useState } from 'react'
+import Arrow from '@/assets/arrowDown_bold.svg?react'
+import styles from './ParamsTableWithExpandle.module.css'
 
 type Props = {
     params: Dictionary<string> | undefined
+    id: string
+    class?: string
+    opened?: boolean
 }
 
 export function ParamsTableWithExpandle(props: Props) {
 
-    const parameters = props.params ? Object.entries(props.params)?.map(([key, value], index) => {
-        return <tr key={index}>
-            <td className='param_name'>{key}</td>
-            <td className='param_text'>{getDisplayValue(key, value)}</td>
-        </tr>
-    }) : undefined
+    const [tables, setTables] = useState<JSX.Element[]>([])
 
-    function getDisplayValue(key: string, value: any) {
-        if (isObject(value)) {
-            if (value.hasOwnProperty('display_value')) {
-                return value.display_value
-            }
-            else
-                return key
-        }
-        else
-            return value
+    useEffect(() => {
+        createTables()
+    }, [props.params])
+
+    function toggleTable(ev: React.MouseEvent<SVGSVGElement, MouseEvent>, tableId: string) {
+        ev.currentTarget.classList.toggle(styles.rotated)
+        ev.currentTarget.parentElement?.parentElement?.classList.toggle(styles.table_opened)
+
+        const table = document.getElementById(tableId)
+        table?.classList.toggle(styles.hidden)
     }
 
-    return (
-        <table className='parameters_table'>
+    function decorateKey(key: string) {
+        key = capitalizeFirstLetter(key)
+        key = key.replaceAll('_', ' ')
+        return key;
+    }
+
+    function createRow(key: string, value: string, tableId: string | undefined = undefined, dark: boolean = false) {
+        return (<tr key={`${props.id}_${key}`} className={dark ? styles.dark_row : undefined}>
+            <td className={styles.param_expand}>{tableId !== undefined && <Arrow className={`${styles.arrow} ${styles.rotated}`} height={15} width={15} onClick={(e) => toggleTable(e, tableId)} />}</td>
+            <td className={styles.param_name}>{decorateKey(key)}</td>
+            <td className={styles.param_text}>{value}</td>
+        </tr>)
+    }
+
+    function createTable(key: string | number, params_row: JSX.Element[]) {
+        return (<table key={`${props.id}_${key}`} className={styles.parameters_table}>
             <colgroup>
-                <col className='param_name_col' />
-                <col className='param_value_col' />
+                <col className={styles.param_expand_col} />
+                <col className={styles.param_name_col} />
+                <col className={styles.param_value_col} />
             </colgroup>
             <tbody>
-                {parameters}
+                {params_row}
             </tbody>
-        </table>
+        </table>)
+    }
+
+    function createTables() {
+        if (!props.params)
+            return
+
+        const tables: JSX.Element[] = []
+        let params_row: JSX.Element[] = []
+
+        const keys = Object.keys(props.params)
+
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i]
+            const value = props.params[keys[i]]
+
+            if (!isObject(value)) {
+                params_row.push(createRow(key, value, undefined, (i + 1) % 2 === 0))
+            }
+            else {
+                if (value.hasOwnProperty('display_value') && value.hasOwnProperty('add_info')) {
+
+                    const tableID = `${props.id}_${key}`
+
+                    params_row.push(createRow(key, value.display_value, tableID, (i + 1) % 2 === 0))
+                    tables.push(createTable(i, params_row))
+
+                    tables.push(<ParamsTableWithExpandle class={styles.inner_table} key={tableID} id={tableID} params={value.add_info} />)
+
+                    params_row = []
+
+                }
+                else {
+                    params_row.push(createRow(key, key, undefined, (i + 1) % 2 === 0))
+                }
+            }
+        }
+
+        tables.push(createTable(`endTable`, params_row))
+        setTables(tables)
+    }
+
+
+    return (
+        <div key={props.id} id={props.id} className={`${!props.opened && styles.hidden} ${props.class}`}>
+            {tables}
+        </div>
     )
 }
