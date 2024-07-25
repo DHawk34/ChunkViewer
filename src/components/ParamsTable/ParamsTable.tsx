72268453d436bb569e9dc1ff261e62f426efb49a
DHawk34 <neobuild@mail.ru>
2024-07-25 16:26:29 +0300

    update ParamsTable

--- src/components/ParamsTable/ParamsTable.tsx
+++ src/components/ParamsTable/ParamsTable.tsx
@@ -1,5 +1,5 @@
 import { Param } from '@/scripts/parsers/sdWebUIParamParser'
-import './ParamsTable.css'
+import styles from './ParamsTable.module.css'
 
 type Props = {
     params: Param[] | undefined
@@ -9,16 +9,16 @@ export function ParamsTable(props: Props) {
 
     const parameters = props.params?.map((param: Param, index: number) => {
         return <tr key={index}>
-            <td className='param_name'>{param.key}</td>
-            <td className='param_text'>{param.value}</td>
+            <td className={styles.param_name}>{param.key}</td>
+            <td className={styles.param_text}>{param.value}</td>
         </tr>
     })
 
     return (
-        <table className='parameters_table'>
+        <table className={styles.parameters_table}>
             <colgroup>
-                <col className='param_name_col' />
-                <col className='param_value_col' />
+                <col className={styles.param_name_col} />
+                <col className={styles.param_value_col} />
             </colgroup>
             <tbody>
                 {parameters}
