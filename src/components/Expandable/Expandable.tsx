acc7400ea50dba110e7b894a29f71dba2887528a
DHawk34 <neobuild@mail.ru>
2024-08-07 17:01:31 +0300

    Add ExpandableGroup & Expandable

--- src/components/Expandable/Expandable.tsx
+++ src/components/Expandable/Expandable.tsx
@@ -1,35 +1,45 @@
 import React, { ReactNode, useEffect, useRef, useState } from "react";
 import Arrow from '@/assets/arrowDown_bold.svg?react'
-import './Expandable.css'
+import styles from './Expandable.module.css'
+import { useExpandContext } from "../ExpandableGroup/ExpandableGroup";
 
 type Props = {
-    header?: string
-    opened?: boolean
+    header: string
     children?: ReactNode
 }
 
 export function Expandable(props: Props) {
 
-    const [opened, setOpened] = useState<boolean>()
+    const expandContext = useExpandContext();
+    const opened = expandContext?.activeExpandable === props.header
 
-    useEffect(() => {
-        setOpened(props.opened ?? false)
-    }, [props.opened])
+    const contentRef = useRef<HTMLDivElement>(null);
+    // const [contentHeight, setHeight] = useState(0)
+
+    // useEffect(() => {
+    //     if (contentRef.current)
+    //         setHeight(contentRef.current.scrollHeight + 10)
+
+    // }, [])
 
     function openCloseExpandable(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-        setOpened(!opened)
+        if (opened)
+            expandContext?.changeActive(undefined)
+        else
+            expandContext?.changeActive(props.header)
     }
 
+    const contentHeight = contentRef.current ? contentRef.current.scrollHeight + 10 : 0
     return (
-        <div className="expandable">
-            <div className='expandable_header' onClick={openCloseExpandable}>
-                <Arrow width="25" height="25" className={`expandable_arrow ${opened ? '' : 'rotated'}`} />
-                <p className='expandable_name'>
+        <div className={styles.expandable}>
+            <div className={`${styles.expandable_header} ${opened ? styles.expanded : ''}`} onClick={openCloseExpandable}>
+                <Arrow width="20" height="20" className={`${styles.expandable_arrow} ${expandContext?.activeExpandable === props.header ? '' : styles.rotated}`} />
+                <p className={styles.expandable_name}>
                     {props.header}
                 </p>
             </div>
-            <div className={`expandable_content ${opened ? 'expanded' : ''}`}>
-                    {props.children}
+            <div ref={contentRef} className={`${styles.expandable_content} ${opened ? styles.expanded : ''}`} style={opened ? { height: contentHeight } : {}}>
+                {props.children}
             </div>
         </div>
     )
