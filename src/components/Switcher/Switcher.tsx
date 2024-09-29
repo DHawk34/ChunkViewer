d10a0459654b37251fc4de0f6dce21b2215f22c1
DHawk34 <neobuild@mail.ru>
2024-09-29 19:47:50 +0300

    Update Switcher.tsx

--- src/components/Switcher/Switcher.tsx
+++ src/components/Switcher/Switcher.tsx
@@ -1,22 +1,27 @@
-import React, { ReactNode, useEffect, useRef, useState } from "react";
+import React, { forwardRef, ReactNode, useEffect, useRef, useState } from "react";
 import Arrow from '@/assets/arrowDown_bold.svg?react'
 import styles from './Switcher.module.css'
 
 type Props = {
-    header?: string
-    opened?: boolean
-    children?: ReactNode
+    defaultChecked?: boolean
+    OnChange?: (newValue: boolean) => void
 }
 
-export function Switcher(props: Props) {
+export const Switcher = forwardRef(function Switcher(props: Props, ref: React.ForwardedRef<HTMLInputElement>) {
+
+    function onSwitcherChange(e: React.ChangeEvent<HTMLInputElement>) {
+        if (props.OnChange)
+            props.OnChange(e.currentTarget.checked)
+    }
+
     return (
         <div className={styles.checkbox_wrapper}>
             <label className={styles.switch}>
-                <input type="checkbox" />
+                <input type="checkbox" ref={ref} defaultChecked={props.defaultChecked} onChange={onSwitcherChange} />
                 <div className={styles.slider}>
                     <div className={styles.round}></div>
                 </div>
             </label>
         </div>
     )
-}
+})
