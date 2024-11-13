c023c36b633dee27a6a0fac4aeeb99d08e5c2af9
DHawk34 <neobuild@mail.ru>
2024-11-13 22:26:09 +0300

    edit ParamsTable

--- src/components/ParamsTableWithExpandle/ParamsTableWithExpandle.tsx
+++ src/components/ParamsTableWithExpandle/ParamsTableWithExpandle.tsx
@@ -24,7 +24,7 @@ export function ParamsTableWithExpandle(props: Props) {
     function copyValue(ev: React.MouseEvent<HTMLParagraphElement, MouseEvent>, value: string) {
         navigator.clipboard.writeText(value);
         toast.success(`Copied to clipboard!`, {
-            id: 'clipboard',
+            // id: 'clipboard',
         });
     }
 
